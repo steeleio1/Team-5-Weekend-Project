902f02d6918e848c1729d7e9e7ad1ffc3830d85a
Ikenna <ikennaugwuh@gmail.com>
2016-07-08 14:07:42 -0400

    Forgot to import jquery.

--- src/js/get-content.js
+++ src/js/get-content.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 function getContent(url){
     return $.ajax('../../app/templates/' + url + '.html');
 }
--- src/js/switch-slide.js
+++ src/js/switch-slide.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 function switchSlide(slide, content){
     $('.contentBox').html(content);
 }
