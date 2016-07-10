ce2186bb420bcf131a82483359ac85915805435f
Ikenna <ikennaugwuh@gmail.com>
2016-07-10 02:00:04 -0400

    Made a function to insert all api data, moved it to its own module, and used a promise to call it only once instead of once for each api request.

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,21 @@
 import $ from 'jquery';
-import get from './get-content'
+import get from './get-content';
 import flickr from './api/flickr';
-import pullData from './api/pull'
+import pullData from './api/pull';
+import insertData from './api/insert';
 import switchTo from './switch-slide';
-import {google_key} from './cred';
 
 var fancyMenuData = 'https://json-data.herokuapp.com/restaurant/menu/1',
     newsData = 'https://json-data.herokuapp.com/restaurant/news/1',
     pubMenuData = 'https://json-data.herokuapp.com/restaurant/menu/3',
-    specialData = 'https://json-data.herokuapp.com/restaurant/news/1';
+    specialData = 'https://json-data.herokuapp.com/restaurant/special/1';
 var mainContent = [];
 
+function insert(data){
+    insertData(mainContent, data);
+}
+
 function populate(flickrPhotos){
-    console.log(flickrPhotos);
     var container = $('.location8Box');
     var Photos = flickrPhotos.photos.photo.splice(1, 3);
     Photos.forEach(function(photo){
@@ -23,45 +26,37 @@ function populate(flickrPhotos){
     });
 }
 
-function insert(data){
-    data = data || {};
-    let map = $('.gMapsBox iframe'),
-        apps = data.appetizers,
-        news = data.post_id;
-    if (map.attr('src') === undefined)
-        map.attr("src", `https://www.google.com/maps/embed/v1/place?key=${google_key}&q=Space+Needle,Seattle+WA`);
-    console.log(data);
-    if (news){
-        $('.location4Box .titleBox p').text(data.title);
-        $('.location4Box .dateBox p').text(data.date_published);
-        $('.location4Box .paragraphBox p').text(data.post);
-    } else if (apps){
-        apps.forEach(function(appetizer){
-
-        });
-    }
+function startPull(){
+    if (!mainContent['story'] || !mainContent['menu'] || !mainContent['reservations'] || !mainContent['games']) return;
+    Promise.all(pullData(specialData, fancyMenuData, newsData, pubMenuData)).then(insert);
+    // pullData(specialData, fancyMenuData, newsData, pubMenuData)
+    //     .forEach(function(request){
+    //         request.then(insert);
+    //     });
 }
 
-get('our-story').then(function(data){ /*switchTo(data);*/ mainContent['story'] = $(data); });
-get('menu').then(function(data){ mainContent['menu'] = $(data); });
-get('reservations').then(function(data){ mainContent['reservations'] = $(data); });
-get('games').then(function(data){ mainContent['games'] = $(data); });
+function storeData(location, data){
+    mainContent[location] = $(data);
+    startPull();
+}
+
+get('our-story').then(function(data){ switchTo(data); storeData('story', data); });
+get('menu').then(function(data){ storeData('menu', data); });
+get('reservations').then(function(data){ storeData('reservations', data); });
+get('games').then(function(data){ storeData('games', data); });
 
 flickr('beer').then(populate);
 flickr('bar food').then(populate);
 flickr('sea scallops dish').then(function(data){
     var photo = data.photos.photo[1];
-    $('.specialsImgBox').html(`<img src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg">`);
+    $('.specialsImgBox').html(`
+        <img src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg">`);
 });
 
-pullData(fancyMenuData).then(insert);
-pullData(pubMenuData).then(insert);
-pullData(newsData).then(insert);
-
 $(document)
-    .delegate('.storyTab', 'click', function(){ switchTo(mainContent['story']); })
-    .delegate('.menuTab', 'click', function(){ switchTo(mainContent['menu']); })
-    .delegate('.resTab', 'click', function(){ switchTo(mainContent['reservations']); })
-    .delegate('.gamesTab', 'click', function(){ switchTo(mainContent['games']); });
+    .delegate('.storyTab', 'mouseenter', function(){ switchTo(mainContent['story']); })
+    .delegate('.menuTab', 'mouseenter', function(){ switchTo(mainContent['menu']); })
+    .delegate('.resTab', 'mouseenter', function(){ switchTo(mainContent['reservations']); })
+    .delegate('.gamesTab', 'mouseenter', function(){ switchTo(mainContent['games']); });
 
-insert();
+insertData();
